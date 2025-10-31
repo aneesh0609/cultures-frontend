ac8fcbd05e9ac6c0bb959d3620918fc3676d9467
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-11-01 00:08:45 +0530

    deployement changes in auth.js and axiosinstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {store} from "../store/store.js"; // import your Redux store
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL  + "/api", 
   withCredentials: true, 
 })
 
