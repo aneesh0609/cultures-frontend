271591e701dc2369c1b37eac3cc382173c0caf4e
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-10-31 23:50:47 +0530

    axiosInstance also updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {store} from "../store/store.js"; // import your Redux store
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: import.meta.env.VITE_API_URL, 
   withCredentials: true, 
 })
 
